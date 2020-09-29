94ab66d6b8abf952bbd99c831fb03b9c4f9d4b31
Miguel A. Peña <miapenahu@unal.edu.co>
2020-09-28 22:04:20 -0500

    Fully functional app for one channel and many superusers

--- communication/settings.py
+++ communication/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #ADDED FOR CHAT APP
+    # ADDED FOR CHAT APP
     'channels',
     'geosmart_club_chat',
 ]
@@ -72,7 +71,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'communication.wsgi.application'
 
-#New configuration for channels
+# New configuration for channels
 ASGI_APPLICATION = "communication.routing.application"
 CHANNEL_LAYERS = {
     'default': {
@@ -88,14 +87,13 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'communication_db',# os.environ.get('DB_NAME'),
-        'USER': 'root', #os.environ.get('DB_USER'),
-        'PASSWORD': 'root', #os.environ.get('DB_PASSWORD'),
-        'HOST': '127.0.0.1', #os.environ.get('DB_HOST'),
-        'PORT': 3306, #os.environ.get('DB_PORT'),
+        'NAME': 'communication_db',  # os.environ.get('DB_NAME'),
+        'USER': 'root',  # os.environ.get('DB_USER'),
+        'PASSWORD': 'root',  # os.environ.get('DB_PASSWORD'),
+        'HOST': '127.0.0.1',  # os.environ.get('DB_HOST'),
+        'PORT': 3306,  # os.environ.get('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -115,7 +113,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -129,8 +126,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
--- geosmart_club_chat/consumers.py
+++ geosmart_club_chat/consumers.py
@@ -1,34 +1,77 @@
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+from .models import Message
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
+class ChatConsumer(WebsocketConsumer):
+
+    def fetch_messages(self, data):
+        messages = Message.last_15_messages()
+        content = {
+            'command': 'messages_dump',
+            'messages': self.messages_to_json(messages)
+        }
+        self.send_message(content)
+
+    def new_message(self, data):
+        author = data['from']
+        author_user = User.objects.filter(username=author)[0]
+        message = Message.objects.create(
+            author=author_user,
+            content=data['message'])
+        content = {
+            'command': 'nuevo_mensaje',
+            'message': self.message_to_json(message)
+        }
+        return self.send_chat_message(content)
+
+    commands={
+        'buscar_mensajes': fetch_messages,
+        'nuevo_mensaje': new_message
+    }
+
+    def messages_to_json(self, messages):
+        result = []
+        for message in messages:
+            result.append(self.message_to_json(message))
+        return result
+
+    def message_to_json(self, message):
+        return {
+            'author': message.author.username,
+            'content': message.content,
+            'timestamp': str(message.timestamp)
+        }
+
+    def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
         # Join room group
-        await self.channel_layer.group_add(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
 
-        await self.accept()
+        self.accept()
 
-    async def disconnect(self, close_code):
+    def disconnect(self, close_code):
         # Leave room group
-        await self.channel_layer.group_discard(
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
     # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+    def receive(self, text_data):
+        data = json.loads(text_data)
+        self.commands[data['command']](self, data)
 
-        # Send message to room group
-        await self.channel_layer.group_send(
+    def send_chat_message(self, message):
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -36,11 +79,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
+    def send_message(self, message):
+        self.send(text_data=json.dumps(message))
     # Receive message from room group
-    async def chat_message(self, event):
-        message = event['message']
 
+    def chat_message(self, event):
+        message = event['message']
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+        self.send(text_data=json.dumps(message))
